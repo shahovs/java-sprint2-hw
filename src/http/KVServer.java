cb0d722b97fb463681012bd6bcf0c74e67311310
shahovs <shahov12345@yandex.ru>
2024-02-19 23:44:44 +0100

    v8.4 fix: незначительные исправления

--- src/http/KVServer.java
+++ src/http/KVServer.java
@@ -41,7 +41,7 @@ public class KVServer {
             if ("GET".equals(h.getRequestMethod())) {
                 String key = h.getRequestURI().getPath().substring("/load/".length());
                 if (key.isEmpty()) {
-                    System.out.println("Key для сохранения пустой. key указывается в пути: /save/{key}");
+                    System.out.println("Key для сохранения пустой. key указывается в пути: /load/{key}");
                     h.sendResponseHeaders(400, 0);
                     return;
                 }
@@ -60,7 +60,7 @@ public class KVServer {
                     e.printStackTrace();
                 }
             } else {
-                System.out.println("/save ждёт POST-запрос, а получил: " + h.getRequestMethod());
+                System.out.println("/load ждёт GET-запрос, а получил: " + h.getRequestMethod());
                 h.sendResponseHeaders(405, 0);
             }
         } catch (IOException e) {
